37a22491953bb7f79ef563d2fda1a6db6b2f7ea1
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-07-31 09:15:15 +0200

    Add Validations  to add customer name

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,9 @@ namespace codeAlongCS.Models
     public class Customer
     {
         public string Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage ="your string should be 10 char")]
+        [DisplayName("Enter your names")]
         public string Name { get; set; }
         public string Telephone { get; set; }
     }
